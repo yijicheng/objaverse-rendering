fd028c65bf59c6892a6bc57316e9e1e313f2ee01
yiji cheng <yiji_cheng@outlook.com>
2024-01-01 13:55:18 +0000

    add v3.5

---
+++ scripts/bak/blender_script_v3.5.py
@@ -0,0 +1,384 @@
+"""Blender script to render images of 3D models.
+
+This script is used to render images of 3D models. It takes in a list of paths
+to .glb files and renders images of each model. The images are from rotating the
+object around the origin. The images are saved to the output directory.
+
+Example usage:
+    blender -b -P blender_script.py -- \
+        --object_path my_object.glb \
+        --output_dir ./views \
+        --engine CYCLES \
+        --scale 0.8 \
+        --num_images 12 \
+        --camera_dist 1.2
+
+Here, input_model_paths.json is a json file containing a list of paths to .glb.
+"""
+
+import argparse
+import math
+import os
+import random
+import sys
+import time
+import urllib.request
+from typing import Tuple
+import numpy as np
+
+working_dir_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(working_dir_path)
+
+
+import bpy
+from mathutils import Vector, Matrix
+
+import blender_utils
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--object_path",
+    type=str,
+    required=True,
+    help="Path to the object file",
+)
+parser.add_argument("--output_dir", type=str, default="./views")
+parser.add_argument(
+    "--engine", type=str, default="CYCLES", choices=["CYCLES", "BLENDER_EEVEE"]
+)
+parser.add_argument("--num_images", type=int, default=30)
+# parser.add_argument("--camera_dist", type=int, default=3.0) # 1.5
+
+# parser.add_argument('--depth_scale', type=float, default=0.25, # 1.0
+#                     help='Scaling that is applied to depth. Depends on size of mesh. Try out various values until you get a good result. Ignored if format is OPEN_EXR.')
+# parser.add_argument('--color_depth', type=str, default='8',
+#                     help='Number of bit per channel used for output. Either 8 or 16.')
+# parser.add_argument('--format', type=str, default='PNG',
+#                     help='Format of files generated. Either PNG or OPEN_EXR')
+
+argv = sys.argv[sys.argv.index("--") + 1 :]
+args = parser.parse_args(argv)
+
+print('===================', args.engine, '===================')
+
+context = bpy.context
+scene = context.scene
+render = scene.render
+
+render.engine = args.engine
+render.image_settings.file_format = "PNG"
+render.image_settings.color_mode = "RGBA"
+render.resolution_x = 1024 # 512
+render.resolution_y = 576 # 512
+render.resolution_percentage = 100
+
+scene.cycles.device = "GPU"
+scene.cycles.samples = 32
+scene.cycles.diffuse_bounces = 1
+scene.cycles.glossy_bounces = 1
+scene.cycles.transparent_max_bounces = 3
+scene.cycles.transmission_bounces = 3
+scene.cycles.filter_width = 0.01
+scene.cycles.use_denoising = True
+scene.render.film_transparent = False # True
+
+bpy.context.preferences.addons["cycles"].preferences.get_devices()
+# Set the device_type
+bpy.context.preferences.addons[
+    "cycles"
+].preferences.compute_device_type = "CUDA" # or "OPENCL"
+
+# scene.use_nodes = True
+# scene.view_layers["ViewLayer"].use_pass_z = True
+# tree = bpy.context.scene.node_tree
+# nodes = tree.nodes
+# links = tree.links
+# # Clear default nodes
+# for n in nodes:
+#     nodes.remove(n)
+# # Create input render layer node
+# render_layers = nodes.new('CompositorNodeRLayers')
+# # Create depth output nodes
+# depth_file_output = nodes.new(type="CompositorNodeOutputFile")
+# depth_file_output.label = 'Depth Output'
+# depth_file_output.base_path = ''
+# depth_file_output.file_slots[0].use_node_format = True
+# depth_file_output.format.file_format = args.format
+# depth_file_output.format.color_depth = args.color_depth
+# depth_file_output.format.color_mode = 'RGBA'
+# depth_file_output.format.compression = 0
+# depth_file_output.format.exr_codec = 'NONE'
+# depth_file_output.format.use_zbuffer = True
+
+# if args.format == 'OPEN_EXR':
+#     links.new(render_layers.outputs['Depth'], depth_file_output.inputs[0])
+# else:
+#     depth_file_output.format.color_mode = "BW"
+
+#     # Remap as other types can not represent the full range of depth.
+#     map = nodes.new(type="CompositorNodeMapValue")
+#     # Size is chosen kind of arbitrarily, try out until you're satisfied with resulting depth map.
+#     map.offset = [-0.7]
+#     map.size = [args.depth_scale]
+#     map.use_min = True
+#     map.min = [0]
+
+#     links.new(render_layers.outputs['Depth'], map.inputs[0])
+#     links.new(map.outputs[0], depth_file_output.inputs[0])
+
+
+def sample_point_on_sphere(radius: float) -> Tuple[float, float, float]:
+    theta = random.random() * 2 * math.pi
+    phi = math.acos(2 * random.random() - 1)
+    return (
+        radius * math.sin(phi) * math.cos(theta),
+        radius * math.sin(phi) * math.sin(theta),
+        radius * math.cos(phi),
+    )
+
+
+def add_lighting() -> None:
+    # delete the default light
+    bpy.data.objects["Light"].select_set(True)
+    bpy.ops.object.delete()
+    # add a new light
+    bpy.ops.object.light_add(type="AREA")
+    light2 = bpy.data.lights["Area"]
+    light2.energy = 30000
+    bpy.data.objects["Area"].location[2] = 0.5
+    bpy.data.objects["Area"].scale[0] = 100
+    bpy.data.objects["Area"].scale[1] = 100
+    bpy.data.objects["Area"].scale[2] = 100
+
+
+def reset_scene() -> None:
+    """Resets the scene to a clean state."""
+    # delete everything that isn't part of a camera or a light
+    for obj in bpy.data.objects:
+        if obj.type not in {"CAMERA", "LIGHT"}:
+            bpy.data.objects.remove(obj, do_unlink=True)
+    # delete all the materials
+    for material in bpy.data.materials:
+        bpy.data.materials.remove(material, do_unlink=True)
+    # delete all the textures
+    for texture in bpy.data.textures:
+        bpy.data.textures.remove(texture, do_unlink=True)
+    # delete all the images
+    for image in bpy.data.images:
+        bpy.data.images.remove(image, do_unlink=True)
+
+
+# load the glb model
+def load_object(object_path: str) -> None:
+    """Loads a glb model into the scene."""
+    if object_path.endswith(".glb"):
+        bpy.ops.import_scene.gltf(filepath=object_path, merge_vertices=True)
+    elif object_path.endswith(".fbx"):
+        bpy.ops.import_scene.fbx(filepath=object_path)
+    else:
+        raise ValueError(f"Unsupported file type: {object_path}")
+
+    obj = bpy.context.object
+    return obj
+
+def scene_bbox(single_obj=None, ignore_matrix=False):
+    bbox_min = (math.inf,) * 3
+    bbox_max = (-math.inf,) * 3
+    found = False
+    for obj in scene_meshes() if single_obj is None else [single_obj]:
+        found = True
+        for coord in obj.bound_box:
+            coord = Vector(coord)
+            if not ignore_matrix:
+                coord = obj.matrix_world @ coord
+            bbox_min = tuple(min(x, y) for x, y in zip(bbox_min, coord))
+            bbox_max = tuple(max(x, y) for x, y in zip(bbox_max, coord))
+    if not found:
+        raise RuntimeError("no objects in scene to compute bounding box for")
+    return Vector(bbox_min), Vector(bbox_max)
+
+
+def scene_root_objects():
+    for obj in bpy.context.scene.objects.values():
+        if not obj.parent:
+            yield obj
+
+
+def scene_meshes():
+    for obj in bpy.context.scene.objects.values():
+        if isinstance(obj.data, (bpy.types.Mesh)):
+            yield obj
+
+# function from https://github.com/panmari/stanford-shapenet-renderer/blob/master/render_blender.py
+def get_3x4_RT_matrix_from_blender(cam):
+    # bcam stands for blender camera
+    # R_bcam2cv = Matrix(
+    #     ((1, 0,  0),
+    #     (0, 1, 0),
+    #     (0, 0, 1)))
+
+    # Transpose since the rotation is object rotation, 
+    # and we want coordinate rotation
+    # R_world2bcam = cam.rotation_euler.to_matrix().transposed()
+    # T_world2bcam = -1*R_world2bcam @ location
+    #
+    # Use matrix_world instead to account for all constraints
+    location, rotation = cam.matrix_world.decompose()[0:2]
+    R_world2bcam = rotation.to_matrix().transposed()
+
+    # Convert camera location to translation vector used in coordinate changes
+    # T_world2bcam = -1*R_world2bcam @ cam.location
+    # Use location from matrix_world to account for constraints:     
+    T_world2bcam = -1*R_world2bcam @ location
+
+    # # Build the coordinate transform matrix from world to computer vision camera
+    # R_world2cv = R_bcam2cv@R_world2bcam
+    # T_world2cv = R_bcam2cv@T_world2bcam
+
+    # put into 3x4 matrix
+    RT = Matrix((
+        R_world2bcam[0][:] + (T_world2bcam[0],),
+        R_world2bcam[1][:] + (T_world2bcam[1],),
+        R_world2bcam[2][:] + (T_world2bcam[2],)
+        ))
+    return RT
+
+def normalize_scene():
+    bbox_min, bbox_max = scene_bbox()
+    scale = 1 / max(bbox_max - bbox_min)
+    for obj in scene_root_objects():
+        obj.scale = obj.scale * scale
+    # Apply scale to matrix_world.
+    bpy.context.view_layer.update()
+    bbox_min, bbox_max = scene_bbox()
+    offset = -(bbox_min + bbox_max) / 2
+    for obj in scene_root_objects():
+        obj.matrix_world.translation += offset
+    bpy.ops.object.select_all(action="DESELECT")
+
+
+def setup_camera():
+    cam = scene.objects["Camera"]
+    cam.location = (0, 1.2, 0)
+    cam.data.lens = 35
+    # cam.data.sensor_width = 32
+    cam.data.sensor_width = 36
+    cam.data.sensor_height = 24
+    cam_constraint = cam.constraints.new(type="TRACK_TO")
+    cam_constraint.track_axis = "TRACK_NEGATIVE_Z"
+    cam_constraint.up_axis = "UP_Y"
+    return cam, cam_constraint
+
+
+def set_principled_node_as_ceramic(principled_node: bpy.types.Node) -> None:
+    blender_utils.set_principled_node(
+        principled_node=principled_node,
+        base_color=(0.8, 0.8, 0.8, 1.0),
+        subsurface=0.1,
+        subsurface_color=(0.9, 0.9, 0.9, 1.0),
+        subsurface_radius=(1.0, 1.0, 1.0),
+        metallic=0.2,
+        specular=0.5,
+        roughness=0.0,
+    )
+
+
+def save_images(object_file: str) -> None:
+    """Saves rendered images of the object in the scene."""
+    os.makedirs(args.output_dir, exist_ok=True)
+    reset_scene()
+    # load the object
+    obj = load_object(object_file)
+    object_uid = os.path.basename(object_file).split(".")[0]
+
+    normalize_scene()
+
+    # # add plane
+    # floor = blender_utils.create_plane(size=1.0, name="Floor")
+    # floor.is_shadow_catcher = False
+    # mat = blender_utils.add_material("Material_Plane", use_nodes=True, make_node_tree_empty=True)
+    # nodes = mat.node_tree.nodes
+    # links = mat.node_tree.links
+    # output_node = nodes.new(type='ShaderNodeOutputMaterial')
+    # principled_node = nodes.new(type='ShaderNodeBsdfPrincipled')
+    # set_principled_node_as_ceramic(principled_node)
+    # links.new(principled_node.outputs['BSDF'], output_node.inputs['Surface'])
+    # floor.data.materials.append(mat)
+
+    # add_lighting()
+    cam, cam_constraint = setup_camera()
+    # create an empty object to track
+    empty = bpy.data.objects.new("Empty", None)
+    scene.collection.objects.link(empty)
+    cam_constraint.target = empty
+
+    world = scene.world
+    blender_utils.build_environment_texture_background(world)
+    ## Lights
+    hdr_list = os.listdir("./HDRIs")
+    blender_utils.set_environment_texture_background(world, os.path.join("./HDRIs", np.random.choice(hdr_list)))
+
+    # set the object position
+    obj.location.y += np.random.uniform(-0.8, 0.8)
+    obj.location.z += np.random.uniform(-0.2, 0.2)
+    # randomize_lighting()
+    camera_dist = np.random.uniform(3.5, 5)
+    for i in range(args.num_images):
+        # set the camera position
+        theta = (i / args.num_images) * math.pi * 2
+        phi = math.radians(75)
+        point = (
+            camera_dist * math.sin(phi) * math.cos(theta),
+            camera_dist * math.sin(phi) * math.sin(theta),
+            camera_dist * math.cos(phi),
+        )
+        cam.location = point
+        # render the image
+        render_path = os.path.join(args.output_dir, object_uid, f"{i:03d}_rgba.png")
+        scene.render.filepath = render_path
+        # # render the depth
+        # depth_file_output.file_slots[0].path = render_path[:-9] + "_depth"
+        # render still
+        bpy.ops.render.render(write_still=True)
+
+        # save camera RT matrix
+        RT = get_3x4_RT_matrix_from_blender(cam)
+        RT_path = os.path.join(args.output_dir, object_uid, f"{i:03d}.npy")
+        np.save(RT_path, RT)
+
+        # os.system(f'mv {render_path[:-9] + "_depth0001.png"} {render_path[:-9] + "_depth.png"}')
+    os.system(f'../azcopy copy "{os.path.join(args.output_dir, object_uid)}" "https://msraimsouthcentralus3.blob.core.windows.net/v-yijicheng/hf-objaverse-v1/{args.output_dir}/?sv=2021-10-04&se=2024-01-28T06%3A04%3A04Z&sr=c&sp=rwl&sig=aUpOHh3UWNqs9w%2BeeuWhYuemv%2Bj11sVdgBxASzqjuEk%3D" --overwrite=prompt --from-to=LocalBlob --blob-type Detect --follow-symlinks --check-length=true --put-md5 --follow-symlinks --disable-auto-decoding=false --recursive --log-level=INFO;')
+    os.system(f'rm -r {os.path.join(args.output_dir, object_uid)}')
+
+
+def download_object(object_url: str) -> str:
+    """Download the object and return the path."""
+    # uid = uuid.uuid4()
+    uid = object_url.split("/")[-1].split(".")[0]
+    tmp_local_path = os.path.join("tmp-objects", f"{uid}.glb" + ".tmp")
+    local_path = os.path.join("tmp-objects", f"{uid}.glb")
+    # wget the file and put it in local_path
+    os.makedirs(os.path.dirname(tmp_local_path), exist_ok=True)
+    urllib.request.urlretrieve(object_url, tmp_local_path)
+    os.rename(tmp_local_path, local_path)
+    # get the absolute path
+    local_path = os.path.abspath(local_path)
+    return local_path
+
+
+if __name__ == "__main__":
+    try:
+        start_i = time.time()
+        if args.object_path.startswith("http"):
+            local_path = download_object(args.object_path)
+        else:
+            local_path = args.object_path
+        save_images(local_path)
+        end_i = time.time()
+        print("Finished", local_path, "in", end_i - start_i, "seconds")
+        # delete the object if it was downloaded
+        if args.object_path.startswith("http"):
+            os.remove(local_path)
+    except Exception as e:
+        print("Failed to render", args.object_path)
+        print(e)
